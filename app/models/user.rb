f04dffa25e6d9bc2d5743bffe260ae9a37b89a06
Ksusha <spas.ksusha@gmail.com>
2022-12-25 15:45:31 +0300

    add validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,9 +16,11 @@ class User < ApplicationRecord
   validates :login, presence: true, uniqueness: true
   validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :login, presence: true, uniqueness: true
-  validates :f_name, presence: true
+  validates :f_name, presence: true, format: { with: /\A[a-zа-яА-ЯA-Z]+\z/, message: 'Имя может содержать только буквы' }
+  validates :l_name, presence: true, format: { with: /\A[a-zа-яА-ЯA-Z]+\z/, message: 'Фамилия может содержать только буквы' }
   validates :password_confirmation, presence: { message: 'не может быть пустым' }
   validates :password, confirmation: { message: 'Пароли не совпадают' }
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).{8,}\z/, message: 'Пароль должен иметь минимум прописную, заглавную буквы, цифру и иметь длину больше 8 символов' }
 
   def email_activate
     self.email_confirmed = true
